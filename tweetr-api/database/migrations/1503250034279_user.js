9b09ca3e4f99b1ba19de910ecaf9dd67e0e1a0b1
felexkemboi <felokemboi10@gmail.com>
2018-06-01 21:15:06 +0300

    add models to the database

--- tweetr-api/database/migrations/1503250034279_user.js
+++ tweetr-api/database/migrations/1503250034279_user.js
@@ -6,9 +6,13 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
+      table.string('name', 80).notNullable().unique()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('location', 60).Nullable()
+      table.string('website_url', 60).Nullable()
+      table.string('bio', 60).Nullable()
       table.timestamps()
     })
   }
